b28a3e480f90177d6f3474c52ec7b2157385ef1d
tricktx <patrick.tx@hotmail.com.br>
2024-03-28 00:27:47 -0300

    testando na cloud 3

--- main.py
+++ main.py
@@ -23,11 +23,17 @@ def download(path):
         prefs,
     )
     options.add_argument("--headless=new")
-    options.add_argument("--no-sandbox")
+    options.add_argument("--test-type")
     options.add_argument("--disable-gpu")
+    options.add_argument("--no-first-run")
+    options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--crash-dumps-dir=/tmp")
-    options.add_argument("--remote-debugging-port=9222")
+    options.add_argument("--no-default-browser-check")
+    options.add_argument("--ignore-certificate-errors")
+    options.add_argument("--start-maximized")
+    options.add_argument(
+        "user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
+    )
     driver = webdriver.Chrome(options=options)
     driver.get('https://dados.gov.br/dados/conjuntos-dados/acessos---banda-larga-fixa')
 
